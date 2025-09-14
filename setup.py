fe506c1952f62365f542681cbb6b9a0a94158d05
snehal-18-tech <snehalthombare2020@gmail.com>
2025-09-14 15:07:37 +0530

    requirements and setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Snehal Thombare",
+    author_email="snehalhombare2020@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
